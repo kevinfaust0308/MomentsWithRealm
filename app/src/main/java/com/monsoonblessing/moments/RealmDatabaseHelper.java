0521be4a44cd59a08d959a02532746ec0ec6e4f6
Kevin Faust <ikevinfaust@gmail.com>
2016-12-26 21:00:54 -0500

    Added documentation complete

--- app/src/main/java/com/monsoonblessing/moments/RealmDatabaseHelper.java
+++ app/src/main/java/com/monsoonblessing/moments/RealmDatabaseHelper.java
@@ -10,21 +10,26 @@ import io.realm.RealmQuery;
 import io.realm.RealmResults;
 
 /**
+ * <p>Helper class for database related operations</p>
  * Updated by Kevin on 2016-12-25.
  */
 public class RealmDatabaseHelper {
-    /*
-    Helper class for database related operations
-     */
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_PHOTO_URI = "photoUri";
+    public static final String COLUMN_DATE_LONG = "dateLong";
+    public static final String COLUMN_MONTH = "month";
+    public static final String COLUMN_YEAR = "year";
 
     private static final String TAG = RealmDatabaseHelper.class.getSimpleName();
 
 
-    //drop down box of all available data year entries for filtering
-    public Set<String> getYears() {
-        /*
-        Get all years of our entries
-         */
+    /**
+     * <p>Get all years of our entries</p>
+     * <p>Used for drop down box of all available data year entries for filtering</p>
+     */
+    public static Set<String> getYears() {
+
 
         Realm realm = Realm.getDefaultInstance();
 
@@ -47,22 +52,23 @@ public class RealmDatabaseHelper {
     }
 
 
-    public RealmResults<MomentModel> searchEntries(SearchPreference searchPreference) {
-        /*
-        Inserts a new row into the database with the passed in data
-         */
-
+    /**
+     * Inserts a new row into the database with the passed in data
+     *
+     * @param searchPreference describes how to filter and sort data
+     */
+    public static RealmResults<MomentModel> searchEntries(SearchPreference searchPreference) {
         Realm realm = Realm.getDefaultInstance();
 
         // Build the query looking at all users:
         RealmQuery<MomentModel> query = realm.where(MomentModel.class);
         // if we have a month filter, add that to the query
         if (searchPreference.getMonth() != null) {
-            query.equalTo("month", searchPreference.getMonth());
+            query.equalTo(COLUMN_MONTH, searchPreference.getMonth());
         }
         // if we have a year filter, add that to the query
         if (searchPreference.getYear() != null) {
-            query.equalTo("year", searchPreference.getYear());
+            query.equalTo(COLUMN_YEAR, searchPreference.getYear());
         }
 
         // execute query for filtered results
@@ -81,11 +87,16 @@ public class RealmDatabaseHelper {
     }
 
 
-    public void insertNewMomentIntoDB(final String title, final String photoUri, final Long date, final String month, final int year) {
-        /*
-        Inserts a new row into the database with the passed in data
-         */
-
+    /**
+     * Inserts a new row into the database with the passed in data
+     *
+     * @param title    title of new entry
+     * @param photoUri photo uri of new entry
+     * @param date     date, in milliseconds, of new entry
+     * @param month    month of new entry
+     * @param year     year of new entry
+     */
+    public static void insertNewMomentIntoDB(final String title, final String photoUri, final Long date, final String month, final int year) {
         Realm realm = Realm.getDefaultInstance();
         realm.executeTransaction(new Realm.Transaction() {
             @Override
@@ -101,18 +112,24 @@ public class RealmDatabaseHelper {
     }
 
 
-    public void updateRow(final int id, final String title, final String photoUri, final Long date, final String month, final int year) {
-        /*
-        Updates a row with the specified data
-         */
-
+    /**
+     * Updates the row with the passed in ID with the new data
+     *
+     * @param id       id of the row to update
+     * @param title    title of new entry
+     * @param photoUri photo uri of new entry
+     * @param date     date, in milliseconds, of new entry
+     * @param month    month of new entry
+     * @param year     year of new entry
+     */
+    public static void updateRow(final int id, final String title, final String photoUri, final Long date, final String month, final int year) {
         Realm realm = Realm.getDefaultInstance();
         realm.executeTransaction(new Realm.Transaction() {
             @Override
             public void execute(Realm realm) {
 
                 // get the row with the specified id
-                MomentModel moment = realm.where(MomentModel.class).equalTo("id", id).findFirst();
+                MomentModel moment = realm.where(MomentModel.class).equalTo(COLUMN_ID, id).findFirst();
                 // update its fields
                 moment.setAllFields(title, photoUri, date, month, year);
 
@@ -123,18 +140,19 @@ public class RealmDatabaseHelper {
     }
 
 
-    public void deleteRow(final int id) {
-        /*
-        Deletes a row from the database with the specified id
-         */
-
+    /**
+     * Deletes the row with the specified id
+     *
+     * @param id id of the row to delete
+     */
+    public static void deleteRow(final int id) {
         Realm realm = Realm.getDefaultInstance();
         realm.executeTransaction(new Realm.Transaction() {
             @Override
             public void execute(Realm realm) {
 
                 // get the row with the specified id
-                MomentModel moment = realm.where(MomentModel.class).equalTo("id", id).findFirst();
+                MomentModel moment = realm.where(MomentModel.class).equalTo(COLUMN_ID, id).findFirst();
                 // delete the row
                 moment.deleteFromRealm();
 
@@ -145,11 +163,10 @@ public class RealmDatabaseHelper {
     }
 
 
-    public void deleteAllEntries() {
-        /*
-        Deletes everything
-         */
-
+    /**
+     * Delete all entries
+     */
+    public static void deleteAllEntries() {
         Realm realm = Realm.getDefaultInstance();
         realm.executeTransaction(new Realm.Transaction() {
             @Override
@@ -167,13 +184,14 @@ public class RealmDatabaseHelper {
     }
 
 
-    private int getNextPrimaryKeyValue() {
-        /*
-        Get the next available primary key value for row insertion
-         */
+    /**
+     * Returns the next available primary key value for row insertion
+     */
+    private static int getNextPrimaryKeyValue() {
+
 
         Realm realm = Realm.getDefaultInstance();
-        Number curr_max = realm.where(MomentModel.class).findAll().max("id");
+        Number curr_max = realm.where(MomentModel.class).findAll().max(COLUMN_ID);
         int new_max = 1;
 
         // if num is not null (meaning we actually have a row), our primary key should have value 1
